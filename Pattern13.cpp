de721911ba2713e8efa9998d85664309785252f3
Juned Ahmad <junedahmad9296@gmail.com>
2023-11-22 10:52:27 +0530

    Add files via upload

---
+++ Pattern13.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    system("clear");
+
+    for (int row = 1; row <= 5; row++)
+    {
+        for (int col = 1; col <= 5 - row + 1; col++)
+        {
+            cout << col << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
